797a72d1f99f30fb3a082869ef5b23b8ce960992
valentin.soltau <valentin.soltau@th-stud.de>
2025-06-30 00:39:02 +0200

    Das Projekt ist mit Maven vorbereitet. Nun kann ich daran arbeiten MongoDBs Datenbank mit Java anzusprechen und zu bearbeiten.

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Character c = new Character("Valle", 24);
-        System.out.println(c.getName());
-    }
-}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Character c = new Character("Valle", 24);
+        System.out.println(c.getName());
+    }
+
+    /*
+    Wo war ich?
+    Ich habe Maven eingerichtet und kann im Terminal mit MongoDB arbeiten.
+    Als nächstes möchte ich durch code auf meine MongoDB zugreifen und z.B. neue User in die
+    appdb einfügen können.
+    Das Ziel ist, so Items und verschiedene game Data zu speichern und abzurufen.
+     */
+
+}
