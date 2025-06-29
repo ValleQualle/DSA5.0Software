55ccaba24d34cbcf70d2c833c67a3ec2e2ba9adf
valentin.soltau <valentin.soltau@th-stud.de>
2025-06-29 23:42:12 +0200

    Das DSA Projekt als Maven Projekt

---
+++ src/main/java/Character.java
@@ -0,0 +1,14 @@
+public class Character {
+
+    private String name;
+    private int age;
+
+    public Character(String n, int a) {
+        this.name = n;
+        this.age = a;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Character c = new Character("Valle", 24);
+        System.out.println(c.getName());
+    }
+}
